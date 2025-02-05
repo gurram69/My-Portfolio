5bdadc3fea0b3ecde854e3feb55a612662bda764
gurram69 <vikku2232002@gmail.com>
2025-02-05 14:59:59 -0500

    changed few file names and added new images with cs dep survey

--- js/app.js
+++ js/app.js
@@ -1,22 +1,38 @@
-$(document).ready(function(){
-    $('.slider').slick({
-        arrows:false,
-        dots:true,
-        appendDots:'.slider-dots',
-        dotsClass:'dots'
+document.addEventListener('DOMContentLoaded', function() {
+    const contactForm = document.getElementById('contactForm');
+    
+    contactForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        const formData = new FormData(contactForm);
+        
+        fetch('https://formspree.io/f/your_formspree_endpoint', {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                alert('Thanks for your submission!');
+                contactForm.reset();
+            } else {
+                alert('Oops! There was a problem submitting your form');
+            }
+        })
+        .catch(error => {
+            alert('Error: ' + error);
+        });
     });
-    
-    
-    let hamberger = document.querySelector('.hamberger');
-    let times = document.querySelector('.times');
-    let mobileNav = document.querySelector('.mobile-nav');
-    
-    hamberger.addEventListener('click', function(){
-      mobileNav.classList.add('open');  
+
+    // Smooth scrolling for navigation links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
     });
-    
-    times.addEventListener('click', function(){
-        mobileNav.classList.remove('open');  
-    });
-    
-    });
+});
